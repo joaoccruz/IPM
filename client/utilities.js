eb45d830b23f2743a559f341f8c21973c446e748
Unknown <joaopinhocruz@gmail.com>
2019-04-03 15:43:02 +0100

    ls updates itself now

--- client/utilities.js
+++ client/utilities.js
@@ -1,6 +1,37 @@
+var currScreen = null;
+var lastScreen = null;
+var intervals = {};
+
 function loadLockScreen(){
 	var d = new Date();
 	var min = d.getMinutes().toString();
 	var hr  = d.getHours().toString();
+	currScreen = "lock";
 	document.getElementById("lockscreenText").innerHTML = hr + ":" + min ;
+}
+
+
+function load(screen){
+	switch(screen){
+		case "lock":
+			loadLockScreen();
+			intervals["lock"] = setInterval(loadLockScreen, 1000);
+	}
+	
+}
+
+function unload(screen){
+	switch(screen){
+		case "lock":
+			clearInterval(intervals["lock"]);
+	}
+}
+
+
+function loadLastScreen(screen){
+	if(lastScreen == null)
+		lastScreen = "main";
+	unload(screen);
+	load(lastScreen);
+	
 }
