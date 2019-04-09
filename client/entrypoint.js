bb12d9200e90fe0ee3a9c2898260708d307d8dcb
Unknown <joaopinhocruz@gmail.com>
2019-04-09 18:23:06 +0100

    refactor stage 2

---
+++ client/entrypoint.js
@@ -0,0 +1,11 @@
+import * as page  from "./js-libs/page.js";
+
+
+var DEBUG_MODE = true;
+
+if(DEBUG_MODE)
+	localStorage.clear();
+
+
+const screenUpdate = setInterval(page.update, 1000);
+page.load("lockscreen");
--- client/js-libs/page.js
+++ client/js-libs/page.js
@@ -1,16 +1,11 @@
-import * as swipe from "./js-libs/swipe.js"
+import * as swipe from "./swipe.js";
 
-var DEBUG_MODE = false;
-var currScreen = null;
-var lastScreen = null;
-var lastScreenShown = null;
-const screenUpdate = setInterval(update, 1000);
-var CURR_POST = 0;
-var POST_LIST = [];
-
-if(DEBUG_MODE)
-	localStorage.clear();
-
+function update(){
+	switch(localStorage.getItem("currScreen")){
+		case "lockscreen":
+			updateLockScreen();
+	}
+}
 
 class post{
 	constructor(image, description, location, handle){
@@ -21,16 +16,73 @@ class post{
 	}
 }
 
+
+var CURR_POST = 0;
+var POST_LIST = [];
+
+
 POST_LIST.push(new post("img/beach.jpeg","Nada como o ar da montanha, na praia",{x: 40.3218825, y: -7.6217218, description: "Serra da Estrela"}, "Senhor_Malaquias"));
 POST_LIST.push(new post("img/montanha.jpg","Imagem genérica de uma montanha",{x: 40.3218825, y: -7.6217218, description: "Montanha"}, "Senhor_José"));
 POST_LIST.push(new post("img/gil.jpg","Grande Gil >.> <.<",{x: 40.3218825, y: -7.6217218, description: "Parque das Nações"}, "Senhor_António"));
 
+
+function load(screen){
+	var tut = localStorage.getItem("tutorial");
+	if( tut == "fingerprint" || tut == undefined) {
+		screen = "tutorial";
+	}
+
+	screen != "lockscreen" ? localStorage.setItem("lastScreen",screen) : null;
+	localStorage.setItem("currScreen",screen);
+
+	document.getElementById(screen).style.display = "block";
+	document.getElementById(screen).style.visibility = "visible";	
+
+	if(screen != "lockscreen" && screen != "tutorial"){
+		loadNotifications();
+	}
+
+	switch(screen){
+		case "lockscreen":
+			updateLockScreen();
+			document.getElementById("lockscreen").addEventListener("click", loadLastScreen, {once: true});
+			break;
+
+		case "main":
+			main();	break;
+
+		case "tutorial":
+			tutorial();	break;
+
+	}
 	
+}
+
+function unload(screen){
+	localStorage.removeItem("currScreen");
+	var ele = document.getElementById(screen);
+	ele.style.visibility = "hidden";
+	ele.style.display = "none"
+	if(screen == "main")
+		swipe.disable(document.getElementById("post"));
+}
+
+function loadLastScreen(){
+	var lastScreen = localStorage.getItem("lastScreen");
+
+	if(lastScreen == undefined)
+		lastScreen = "main";
+
+	alert(lastScreen == null);
+	unload(localStorage.getItem("currScreen"));
+	load(lastScreen);
+}
 
 
-function runTutorial(){
-	var tutorial = document.getElementById("tutorial");
 
+
+function tutorial(){
+	var tut = document.getElementById("tutorial");
 	var t1 = document.getElementById("tutorialText1");
 	var t2 = document.getElementById("tutorialText2");
 	var skip = document.getElementById("skip");
@@ -39,25 +91,25 @@ function runTutorial(){
 
 	switch(localStorage.getItem("tutorial")){
 		case null:
-			lastScreenShown = "tutorialNull";
-			tutorial.addEventListener("click", function(){
+			localStorage.setItem("lastScreen","tutorial");
+			tut.addEventListener("click", function(){
 				t1.innerHTML = "Place one of your fingers on the screen to configure the fingerprint sensor";
 				t2.innerHTML = "";
 				skip.innerHTML = ""
 				i3.src = "img/fingerprint.png";
 				localStorage.setItem("tutorial", "fingerprint");
-				runTutorial();
+				tutorial();
 			}, {once : true});
 			break;
 
 		case "fingerprint":
-			tutorial.addEventListener("click", function(){		
+			tut.addEventListener("click", function(){		
 				t1.innerHTML = "The next few screens will be a tutorial on how to use the device";
 				t2.innerHTML = "You can skip them if you have used this before"
 				i3.src = "";
 				skip.innerHTML = "skip";
 				localStorage.setItem("tutorial", "tutorial1");
-				runTutorial();
+				tutorial();
 			}, {once : true});
 			break;
 
@@ -69,7 +121,7 @@ function runTutorial(){
 				load("main");
 			}, {once : true});
 
-			tutorial.addEventListener("click", function(){
+			tut.addEventListener("click", function(){
 				if(localStorage.getItem("tutorial") == "complete")
 					return;
 				t1.innerHTML = "";
@@ -77,7 +129,7 @@ function runTutorial(){
 				i3.src = "";
 				skip.innerHTML = "skip";
 				localStorage.setItem("tutorial", "tutorialMain");
-				runTutorial();
+				tutorial();
 			}, {once : true});
 
 
@@ -100,15 +152,6 @@ function runTutorial(){
 }
 
 
-function update(){
-	switch(currScreen){
-		case "lockscreen":
-			updateLockScreen();
-	}
-}
-
-
-
 function updateLockScreen(){
 	var d = new Date();
 	var min = d.getMinutes().toString();
@@ -119,15 +162,13 @@ function updateLockScreen(){
 	
 	if(hr < 10)
 		hr = "0" + hr
-	
-	currScreen = "lockscreen";
+
 	document.getElementById("lockscreenText").innerHTML = hr + ":" + min ;
+
 }
 
 
-
-
-function loadMain(){
+function main(){
 	function drawPost(ID){ 
 		document.getElementById("mainImage").src = POST_LIST[ID].image;
 		document.getElementById("postDescription").innerHTML = POST_LIST[ID].description;
@@ -150,56 +191,13 @@ function loadMain(){
 	swipe.enable(document.getElementById("post"),["left","right"],[loadNext,loadPrev]);
 }
 
-function load(screen){
-	var tut = localStorage.getItem("tutorial");
-	if( tut == "fingerprint" || tut == null) {
-		screen = "tutorial";
-	}
-
-	lastScreenShown = screen;
-
-	currScreen = screen;
-	document.getElementById(screen).style.display = "block";
-	document.getElementById(screen).style.visibility = "visible";	
-	if(screen != "lockscreen" && screen != "tutorial"){
-		document.getElementById("notifications").style.display = "block";
-		document.getElementById("notifications").style.visibility = "visible";	
-	}
-
-	switch(screen){
-		case "lockscreen":
-			updateLockScreen();
-			document.getElementById("lockscreen").addEventListener("click", loadLastScreen, {once: true});
-			break;
-
-		case "main":
-			loadMain();
-			break;
-
-		case "tutorial":
-			runTutorial();
-			break;
-
-	}
-	
-}
-
-function unload(screen){
-	currScreen = null;
-	var ele = document.getElementById(screen);
-	ele.style.visibility = "hidden";
-	ele.style.display = "none"
-	if(screen == "main")
-		swipe.disable(document.getElementById("post"));
+function loadNotifications(){
+	document.getElementById("notifications").style.display = "block";
+	document.getElementById("notifications").style.visibility = "visible";	
 }
 
 
-function loadLastScreen(){
-	if(lastScreen == null)
-		lastScreen = "main";
-	unload(currScreen);
-	load(lastScreen);
-}
 
 
-load("lockscreen");
+
+export {load};
