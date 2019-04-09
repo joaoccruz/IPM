a38f684e345aabdacf13c6df6cd1fca14b55cb3a
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2019-04-09 15:38:44 +0100

    Wanna go commit die

--- client/anime-utils.js
+++ client/anime-utils.js
@@ -1,15 +1,17 @@
 // OWO
 
-import anime from 'js-libs/anime-master/lib/anime.es.js';
+import tween from "./js-libs/anime-master/lib/anime.es.js";
 
 function a() {
-	anime({
+	console.log("hi!")
+	tween.anime({
 		targets: '#fingerprint',
 		width: '25%'
 	});
 }
 function b() {
-	anime({
+	console.log("bye!")
+	tween.anime({
 		targets: '#fingerprint',
 		width: '20%'
 	});
