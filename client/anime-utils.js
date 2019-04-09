f0dc4823ddd402bb21f7d542c0ca00fc434af85d
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2019-04-09 09:04:18 +0100

    Began playing with anime.js
    Currently the functions aren't being ran for some reason (?) - got to investigate later.

---
+++ client/anime-utils.js
@@ -0,0 +1,16 @@
+// OWO
+
+import anime from 'js-libs/anime-master/lib/anime.es.js';
+
+function a() {
+	anime({
+		targets: '#fingerprint',
+		width: '25%'
+	});
+}
+function b() {
+	anime({
+		targets: '#fingerprint',
+		width: '20%'
+	});
+}
--- client/utilities.js
+++ client/utilities.js
@@ -1,6 +1,3 @@
-// Proudly using anime.js
-// Owo what's this?
-
 var DEBUG_MODE = false;
 var currScreen = null;
 var lastScreen = null;
@@ -8,8 +5,6 @@ var lastScreenShown = null;
 var screenUpdate = setInterval(update, 1000);
 
 var tutorialEventListeners=[];
-
-// const anime = require('js-libs/anime-master/lib/anime.js');
 
 class post{
 	constructor(image, description, location, locationName){
@@ -21,9 +16,9 @@ class post{
 }
 
 var POST_LIST = [];
-POST_LIST.push(new post("img/beach.jpeg","Nada como o ar da montanha, na praia",{x: 40.3218825, y: -7.6217218}, "Serra da Estrela"));
-POST_LIST.push(new post("img/montanha.jpg","Imagem genéria de uma montanha",{x: 40.3218825, y: -7.6217218}, "Montanha"));
-POST_LIST.push(new post("img/gil.jpg","Grande Gil",{x: 40.3218825, y: -7.6217218}, "Parque das Nações"));
+POST_LIST.push(new post("img/beach.jpeg","Nada como o ar da montanha, na praia... Wait.",{x: 40.3218825, y: -7.6217218}, "Serra da Estrela"));
+POST_LIST.push(new post("img/montanha.jpg","Imagem genérica de uma montanha",{x: 40.3218825, y: -7.6217218}, "Montanha"));
+POST_LIST.push(new post("img/gil.jpg","Grande Gil >.> <.<",{x: 40.3218825, y: -7.6217218}, "Parque das Nações"));
 
 if(DEBUG_MODE)
 	localStorage.clear();
@@ -179,5 +174,4 @@ function loadLastScreen(screen){
 		lastScreen = "main";
 	unload(screen);
 	load(lastScreen);
-	
 }
